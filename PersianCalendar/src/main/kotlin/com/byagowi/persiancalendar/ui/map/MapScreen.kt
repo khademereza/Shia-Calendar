92553b50ec7a27b74b30c18a8f3e11685db69db0
Ebrahim <ebrahim@gnu.org>
2026-07-03 01:33:25 +0330

    map: Bring bottom sheet for types selection
    Specially as HamidReza expressed interest on resolving its issue instead of
    the removal.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapScreen.kt
@@ -36,9 +36,12 @@ import androidx.compose.material.icons.filled.MyLocation
 import androidx.compose.material.icons.filled.NightlightRound
 import androidx.compose.material.icons.filled.SocialDistance
 import androidx.compose.material.icons.filled._3dRotation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
@@ -83,7 +86,6 @@ import com.byagowi.persiancalendar.global.coordinates
 import com.byagowi.persiancalendar.global.language
 import com.byagowi.persiancalendar.global.numeral
 import com.byagowi.persiancalendar.global.showQibla
-import com.byagowi.persiancalendar.ui.common.AppDialog
 import com.byagowi.persiancalendar.ui.common.AppIconButton
 import com.byagowi.persiancalendar.ui.common.DatePickerDialog
 import com.byagowi.persiancalendar.ui.common.NavigationNavigateUpIcon
@@ -157,7 +159,8 @@ fun SharedTransitionScope.MapScreen(
     )
 
     var showMapTypesDialog by rememberSaveable { mutableStateOf(false) }
-    if (showMapTypesDialog) AppDialog(
+    if (showMapTypesDialog) @OptIn(ExperimentalMaterial3Api::class) ModalBottomSheet(
+        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
         onDismissRequest = { showMapTypesDialog = false },
     ) {
         MapType.entries.drop(1) // Hide "None" option
